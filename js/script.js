f28bcfe05eb584d1e51eed54746a9cf45b72057e
itliza <elizaveta.romaniuk@gmail.com>
2021-12-01 21:06:38 +0200

    wrapper-shrink

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 function toggleMenu() {
-  let menuInfo = document.getElementById('info');    
+  let menuInfo = document.getElementById('info');  
+  let wrapperShrink = document.getElementById('wrapper');
+
   if(menuInfo.style.display == "none") { // if is menuInfo displayed, hide it
     menuInfo.style.display = "block";
+    wrapperShrink.classList.add('wrapper-shrink');
   }
   else { // if is menuInfo hidden, display it
     menuInfo.style.display = "none";
+    wrapperShrink.classList.remove('wrapper-shrink');
   }
 }
