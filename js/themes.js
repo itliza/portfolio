f17f75afeb13042f3f01f8de94fec02666046c65
itliza <elizaveta.romaniuk@gmail.com>
2021-12-02 09:25:13 +0200

    theme button

--- js/themes.js
+++ js/themes.js
@@ -1,28 +1,61 @@
-var btn = document.getElementById("theme-button");
-var link = document.getElementById("theme-link");
+// var btn = document.getElementById("theme-button");
+// var link = document.getElementById("theme-link");
 
-btn.addEventListener("click", function () { ChangeTheme(); });
+// btn.addEventListener("click", function () { ChangeTheme(); });
 
-function ChangeTheme()
-{
-    let lightTheme = "css/light.css";
-    let darkTheme = "css/dark.css";
+// function ChangeTheme()
+// {
+//     let lightTheme = "css/light.css";
+//     let darkTheme = "css/dark.css";
 
-    var currTheme = link.getAttribute("href");
-    var theme = "";
+//     var currTheme = link.getAttribute("href");
+//     var theme = "";
 
-    if(currTheme == lightTheme)
-    {
-     currTheme = darkTheme;
-     theme = "dark";
-    }
-    else
-    {    
-     currTheme = lightTheme;
-     theme = "light";
-    }
+//     if(currTheme == lightTheme)
+//     {
+//      currTheme = darkTheme;
+//      theme = "dark";
+//     }
+//     else
+//     {    
+//      currTheme = lightTheme;
+//      theme = "light";
+//     }
 
+//     link.setAttribute("href", currTheme);
+
+//     Save(theme);
+// }
+
+
+
+        const checkbox = document.querySelector('input[name=theme]'),
+              btn = document.getElementById("theme-button"),
+              link = document.getElementById("theme-link");
+
+        checkbox.addEventListener('change', function() {
+            let lightTheme = "css/light.css";
+            let darkTheme = "css/dark.css";
+            var currTheme = link.getAttribute("href");
+            var theme = "";
+            if(this.checked) {
+                trans()
+                // document.documentElement.setAttribute('data-theme', 'dark')
+                currTheme = darkTheme;
+                theme = "dark";
+            } else {
+                trans()
+                // document.documentElement.setAttribute('data-theme', 'light')
+                currTheme = lightTheme;
+                theme = "light";
+            }
     link.setAttribute("href", currTheme);
 
-    Save(theme);
-}
+        })
+
+        let trans = () => {
+            document.documentElement.classList.add('transition');
+            window.setTimeout(() => {
+                document.documentElement.classList.remove('transition')
+            }, 1000)
+        }
