0387c39b0e60d7c7c9dab5e7fa6f6d42432b1b33
Yelizaveta Romaniuk <elizaveta.romaniuk@gmail.com>
2025-05-14 13:08:21 +0300

    add calendar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 function toggleMenu() {
-  let menuInfo = document.getElementById('info');  
-  let wrapperShrink = document.getElementById('wrapper');
+  let menuInfo = document.getElementById("info");
+  let wrapperShrink = document.getElementById("wrapper");
 
-  if(menuInfo.style.display == "none") { // if is menuInfo displayed, hide it
+  if (menuInfo.style.display == "none") {
     menuInfo.style.display = "block";
-    wrapperShrink.classList.add('wrapper-shrink');
-  }
-  else { // if is menuInfo hidden, display it
+    wrapperShrink.classList.add("wrapper-shrink");
+  } else {
     menuInfo.style.display = "none";
-    wrapperShrink.classList.remove('wrapper-shrink');
+    wrapperShrink.classList.remove("wrapper-shrink");
   }
 }
